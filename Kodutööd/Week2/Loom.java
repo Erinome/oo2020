36e5352a1262814af5a299c7359ab641422ac21b
erinome <rebanee@tlu.ee>
2020-02-18 08:53:33 +0200

    Kodutöö

---
+++ Kodutööd/Week2/Loom.java
@@ -0,0 +1,17 @@
+public class Loom {
+
+    String animal_type;
+
+    public void catMeows(){
+        System.out.println("Mina olen kass, njäuu");
+    }
+
+    public void dogBarks(){
+        System.out.println("Mina olen koer, haugun");
+    }
+
+    /*void setAnimalType(String animal_type) {
+        this.animal_type = animal_type;
+    }*/
+
+}
---
+++ Kodutööd/Week2/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+            Loom cat = new Loom();
+            cat.setAnimalType(cat);
+            System.out.println(cat.catMeows());
+
+            
+    }
+}
