7bce395f4a3bad381a659d7c0c54c6f4d1e5b37f
Raner Rebane <7990409+Erinome@users.noreply.github.com>
2020-05-25 12:19:07 +0300

    Add files via upload

---
+++ w09_tunnirakenduse_lahendus/javaspring/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Greeting {
+
+  private String content;
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
